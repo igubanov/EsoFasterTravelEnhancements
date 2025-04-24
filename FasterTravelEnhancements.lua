5fa3ff29ada9ea53aac4232b7fed327928662541
Ivan G <ivan.gubanov@abv.bg>
2025-04-24 23:45:01 +0300

    add feature to hide FasterTravel keybinds on the map

--- FasterTravelEnhancements.lua
+++ FasterTravelEnhancements.lua
@@ -3,7 +3,10 @@ FasterTravelEnhancements = { addon = { name = "FasterTravelEnhancements" } }
 local FT = FasterTravel
 local FTE = FasterTravelEnhancements
 local addon = FTE.addon
-local savedVars
+local savedVars = {
+    hideMapKeybind = nil, --- @type boolean
+    lastTab = nil         --- @type string
+}
 local FtCategoryNames = {
     All = GetString(FASTER_TRAVEL_WAYSHRINES_CATEGORY_ALL),
     Favorites = GetString(FASTER_TRAVEL_WAYSHRINES_CATEGORY_FAVOURITES),
@@ -56,53 +59,77 @@ local function FixZoneIndexForFavoriteAndRecent()
 end
 
 --- save last tab NAME and try to find and restore after game restart
---- probably in the future may change to tab index/number (but it'savedVars required cycle)
+--- probably in the future may change to tab index/number (but it's required cycle each time when close map)
 local function FixLastTab()
-    if (savedVars.lastTab ~= nil) then
-        local originalFunc = WORLD_MAP_INFO.SelectTab;
-        WORLD_MAP_INFO.SelectTab = FasterTravel.hook(WORLD_MAP_INFO.SelectTab,
-            function(base, self, tabId)
-                local lastTabId = tabId
-                for _, btn in ipairs(WORLD_MAP_INFO.modeBar.buttonData) do
-                    local tabName = GetString(btn.descriptor)
-                    if (tabName == savedVars.lastTab) then
-                        lastTabId = btn.descriptor
-                        break
-                    end
-                end
-
-                base(self, lastTabId)
-                -- d("SelectTab hooked, restore " .. lastTabId)
-                WORLD_MAP_INFO.SelectTab = originalFunc
-            end)
-    end
-
+    -- always save last selected map tab
     SCENE_MANAGER:GetScene('worldMap'):RegisterCallback("StateChange",
         function(oldState, newState)
             if (newState ~= SCENE_HIDDEN) then return end
             savedVars.lastTab = GetString(WORLD_MAP_INFO.modeBar.lastFragmentName)
-            -- d("LastTab saved " .. savedVars.lastTab)
+        end)
+
+
+
+    local function findMapTabDescriptorByTabName(tabName)
+        for _, btn in ipairs(WORLD_MAP_INFO.modeBar.buttonData) do
+            local currentName = GetString(btn.descriptor)
+            if (tabName == currentName) then
+                return btn.descriptor
+            end
+        end
+    end
+
+    -- optional restore previous saved tab (only if already selected )
+    local isLastTabSettingEnabled =
+        FT.Options.initial_tab[FT.settings.initial_tab].value == FASTER_TRAVEL_SETTINGS_INITIAL_TAB_LAST
+
+
+    if (isLastTabSettingEnabled and savedVars.lastTab ~= nil) then
+        WORLD_MAP_INFO.SelectTab = FT.hook(WORLD_MAP_INFO.SelectTab,
+            function(base, self, tabId)
+                local tabIdToOpen = findMapTabDescriptorByTabName(savedVars.lastTab) or tabId
+                WORLD_MAP_INFO.SelectTab = base
+                return base(self, tabIdToOpen)
+            end)
+    end
+end
+
+--- that functions hook map keybindigs from FasterTravel
+--- and override visible function - add new condition - disable button flag is not active
+local function HideFastTravelMapKeybinding()
+    KEYBIND_STRIP.AddKeybindButtonGroup = FT.hook(KEYBIND_STRIP.AddKeybindButtonGroup,
+        function(base, self, btn)
+            if (btn[1].keybind == "FASTER_TRAVEL_REJUMP") then
+                for _, button in ipairs(btn) do
+                    if (button.visible ~= nil) then
+                        button.visible = FT.hook(button.visible,
+                            function(base, ...) return not savedVars.hideMapKeybind and base(...) end)
+                    end
+                end
+
+                KEYBIND_STRIP.AddKeybindButtonGroup = base
+            end
+
+            return base(self, btn)
         end)
 end
 
 local function OnAddOnLoaded(event, addonName)
     if addonName ~= addon.name then return end
 
-    savedVars = ZO_SavedVars:NewAccountWide(addon.name .. "_Data", 1, nil, {})
-    FTE.savedVars = savedVars
-
-    local ftTabSetting = FT.Options.initial_tab[FT.settings.initial_tab].value
-    if (ftTabSetting == FASTER_TRAVEL_SETTINGS_INITIAL_TAB_LAST) then
-        FixLastTab()
-    end
-
-    FixZoneIndexForFavoriteAndRecent()
-
     if not FT then
         zo_callLater(function() d(addon.name .. " cant work without FasterTravel") end, 5000)
         return
     end
+
+    savedVars = ZO_SavedVars:NewAccountWide(addon.name .. "_Data", 1, nil, savedVars)
+    FTE.savedVars = savedVars
+
+    FixLastTab()
+    FixZoneIndexForFavoriteAndRecent()
+    HideFastTravelMapKeybinding()
 end
+
 
 -- override init method asap to hook creation of object
 FT.MapTabWayshrines.init = FT.hook(FT.MapTabWayshrines.init,
